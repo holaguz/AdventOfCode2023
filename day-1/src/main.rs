a04f2444f6cb3c1de627b07c78ac46420097a0bc
Agustin Catellani <agustin.catellani@gmail.com>
2023-12-14 20:05:56 -0300

    day 1 rewrite

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,49 +1,50 @@
-const TEST_INPUT : &str = "two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen";
+use std::io::{self, BufRead};
 
-const MAP: [(&str, u32); 10] = [
-    ("one", 1),
-    ("two", 2),
-    ("three", 3),
-    ("four", 4),
-    ("five", 5),
-    ("six", 6),
-    ("seven", 7),
-    ("eight", 8),
-    ("nine", 9),
-    ("zero", 0),
+const MAP: [(&str, char); 10] = [
+    ("one", '1'),
+    ("two", '2'),
+    ("three", '3'),
+    ("four", '4'),
+    ("five", '5'),
+    ("six", '6'),
+    ("seven", '7'),
+    ("eight", '8'),
+    ("nine", '9'),
+    ("zero", '0'),
 ];
 
+fn process(s: &str) -> u32 {
+    let nums = s.chars().filter(|c| c.is_digit(10)).collect::<String>();
+    [
+        nums.chars().nth(0).unwrap(),
+        nums.chars().nth_back(0).unwrap(),
+    ]
+    .iter()
+    .collect::<String>()
+    .parse::<u32>()
+    .expect("Empty")
+}
+
+fn convert(s: &str) -> String {
+    let mut new = String::new();
+    for i in 0..s.len() {
+        MAP.iter().for_each(|(ns, n)| {
+            if s[i..].starts_with(ns) {
+                new.push(*n);
+            } else {
+                new.push(s.chars().nth(i).unwrap());
+            }
+        });
+    }
+    new
+}
+
 fn main() {
+    let input: Vec<String> = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
 
-    let mut input = TEST_INPUT.to_string().replace('\n', " ");
-    // let mut input = std::fs::read_to_string("input").unwrap().replace('\n', " ");
+    let ans = input.iter().fold(0, |acc, l| acc + process(l));
+    println!("Part 1: {}", ans);
 
-    println!("{}\n", input);
-
-    let test = input.matches("two");
-    println!("{test:?}\n");
-
-    MAP.iter().for_each(|s| {
-        input = input.replace(s.0, &s.1.to_string());
-    });
-    
-    println!("{}\n", input);
-
-
-    let split: String = input.chars().filter(|c| c.is_numeric() || *c == ' ' ).collect();
-    println!("{split:?}\n");
-
-    let x: Vec<u32>  = split.split_whitespace().map(|s| { 
-        s.chars().nth(0).unwrap().to_string() + &s.chars().nth_back(0).unwrap().to_string() })
-        .map(|s| s.parse::<u32>().unwrap()).collect();
-
-    let part1: u32 = x.iter().sum();
-
-    println!("{}\n", part1);
+    let ans = input.iter().fold(0, |acc, l| acc + process(&convert(l)));
+    println!("Part 2: {}", ans);
 }
